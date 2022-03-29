2ed7eff0a76179c3b8dbb086353a694ca8a725fd
leo <carl-leo-benjamin@hotmail.com>
2022-03-29 10:18:08 +0200

    tomt projekt ny

--- src/main/java/com/company/repositories/ProductRepository.java
+++ src/main/java/com/company/repositories/ProductRepository.java
@@ -2,6 +2,6 @@ package com.company.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<, Integer> {
+//public interface ProductRepository extends CrudRepository<, Integer> {
 
-}
+//}
