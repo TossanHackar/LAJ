f93576c6574200029409763295a9c5673876f6ce
leo <carl-leo-benjamin@hotmail.com>
2022-03-29 09:58:31 +0200

    tomt projekt

---
+++ src/main/java/com/company/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.company.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<, Integer> {
+
+}
--- src/test/java/com/company/WebAppApplicationTests.java
+++ src/test/java/com/company/WebAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ensat;
+package com.company;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
